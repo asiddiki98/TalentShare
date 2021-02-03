cdf2175a334647ac4e933552ba8bcf951fd0b04f
Paul Ramirez <pramirez7230@bths.edu>
2021-02-02 23:15:16 -0500

    Create post item, index

---
+++ frontend/src/components/posts/postItemContainer.js
@@ -0,0 +1,9 @@
+import { connect } from 'react-redux';
+import PostItem from './postItem';
+import { } from '../../actions/post_actions';
+
+const mDTP = state => {
+  
+}
+
+export default connect(null, mDTP)(PostItem)
