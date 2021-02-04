2b4ef69e2decf0972fa0061d2e1d64d7fc97653e
Paul Ramirez <pramirez7230@bths.edu>
2021-02-04 18:13:04 -0500

    Update and refine splash styling

--- frontend/src/components/session/splash.jsx
+++ frontend/src/components/session/splash.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 // import { AuthRoute, ProtectedRoute } from '../../util/route_util';
 import LoginFormContainer from '../session/login_form_container';
 import WhiteLogo from '../../assets/images/talentsharelight.png';
+import Tagline from '../../assets/images/tagline.png';
 import SplashBackground from '../../assets/images/splash-background.png';
 import '../../assets/splash.css'
 const Splash = () => {
@@ -11,6 +12,7 @@ const Splash = () => {
     <div className="splash-page">
       <div id="splash-design-container">
         <img id="splash-logo" src={WhiteLogo} alt="Splash Logo"/>
+        <img id="tagline" src={Tagline} alt="Tagline"/>
       </div>
       <LoginFormContainer id="login-form"/>
     </div>
