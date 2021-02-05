6c3d883f8bcc686977d9c9e37b700e2a64b0b55b
Stephen <syangrea@gmail.com>
2021-02-05 01:51:11 -0500

    fix prod issues

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(cors());
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
     cors: {
-        origin: "http://localhost:3000",
+        
         methods: ["GET", "POST"],
         credentials: true
     }
