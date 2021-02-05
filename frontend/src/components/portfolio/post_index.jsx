a172ed0333da68bcc1bf8b6f998c39cef683fa8a
Stephen <syangrea@gmail.com>
2021-02-05 11:03:58 -0500

    optimize categories and portfolio

--- frontend/src/components/portfolio/post_index.jsx
+++ frontend/src/components/portfolio/post_index.jsx
@@ -3,10 +3,11 @@ import PostItem from './post_item';
 
 export default class PostsIndex extends React.Component {
     componentDidMount() {
-        this.props.fetchUserPosts(this.props.user._id);
+        // this.props.fetchUserPosts(this.props.user._id);
     }
    
     render() {
+        debugger
         return (
             <div className="posts-index-container">
                 {this.props.posts.map(post => <PostItem key={post._id} post={post} viewPost={this.props.viewPost} />)}
