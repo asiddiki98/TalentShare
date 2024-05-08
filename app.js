3f3db6e772268c45e19bdac2ca8b6c4bef4731fd
asiddiki98 <azimsardines@gmail.com>
2024-05-08 12:26:49 -0400

    fix: updated 1 package and fixed bug in navbar_container, and made new port 5002

--- app.js
+++ app.js
@@ -1,111 +1,105 @@
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 const users = require("./routes/api/users");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const User = require("./models/User");
-const cors = require('cors');
-const keys = require('./config/keys');
+const cors = require("cors");
+const keys = require("./config/keys");
 const path = require("path");
-const Post = require('./models/Post');
+const Post = require("./models/Post");
 const posts = require("./routes/api/posts");
-const Message = require('./models/Message');
-const messages = require('./routes/api/messages')
+const Message = require("./models/Message");
+const messages = require("./routes/api/messages");
 const comments = require("./routes/api/comments");
-const passport = require('passport');
+const passport = require("passport");
 
-const crypto = require('crypto');
-const methodOverride = require('method-override');
+const crypto = require("crypto");
+const methodOverride = require("method-override");
 const multer = require("multer");
-const GridFsStorage = require('multer-gridfs-storage');
+const GridFsStorage = require("multer-gridfs-storage");
 const Grid = require("gridfs-stream");
-const { pathToFileURL } = require('url');
-const imageRouter = require('./routes/image');
+const { pathToFileURL } = require("url");
+const imageRouter = require("./routes/image");
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
 }
 
-
 app.use(cors());
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const port = process.env.PORT || 5000;
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const port = process.env.PORT || 5002;
 server.listen(port, () => console.log(`Server is running on port ${port}`));
 
-io.sockets.on('connection', function(socket){
-    
-    console.log('user is connected');
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
+io.sockets.on("connection", function (socket) {
+  console.log("user is connected");
+  socket.on("disconnect", function () {
+    console.log("user disconnected");
+  });
+  socket.on("connect user", socket.join);
+  socket.on("disconnect user", socket.leave);
+
+  socket.on("chat message", function (msg) {
+    const newMessage = new Message(msg);
+    newMessage.save().then((msg) => {
+      io.emit("chat message", msg);
+      io.emit("chat message", msg);
     });
-    socket.on("connect user", socket.join);
-    socket.on("disconnect user", socket.leave);
-    
-    socket.on('chat message', function(msg) {
-        const newMessage = new Message(msg)
-        newMessage.save().then(msg => {
-            io.emit('chat message', msg)
-            io.emit('chat message', msg)
-        })
-        
-    })
-})
+  });
+});
 
 // http.listen(process.env.NODE_ENV === 'production' ? "http://talentshare-aa.herokuapp.com/" : 8000);
 
-
 mongoose
-.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log("Connected to MongoDB successfully"))
-.catch(err => console.log(err));
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.log(err));
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
 app.use(passport.initialize());
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
 app.use("/api/users", users);
 
 const storage = new GridFsStorage({
-    url: db,
-    file: (req, file) => {
-        return new Promise((resolve, reject) => {
-            crypto.randomBytes(16, (err, buf) => {
-                if (err) return reject(err);
-                const filename = buf.toString('hex') + path.extname(file.originalname);
-                const fileInfo = {
-                    filename: filename,
-                    bucketName: 'uploads'
-                };
-                resolve(fileInfo);
-            })
-        })
-    }
-})
+  url: db,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) return reject(err);
+        const filename = buf.toString("hex") + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: "uploads",
+        };
+        resolve(fileInfo);
+      });
+    });
+  },
+});
 
-const upload = multer({storage});
+const upload = multer({ storage });
 
-
-app.use('/content', imageRouter(upload));
-app.use("/api/posts", posts)
-app.use('/api/messages', messages)
+app.use("/content", imageRouter(upload));
+app.use("/api/posts", posts);
+app.use("/api/messages", messages);
 
 app.use("/api/comments", comments);
 
-
 // const port = process.env.PORT || 5000;
 // app.listen(port, () => console.log(`Server is running on port ${port}`));
 // const port = process.env.PORT || 5000;
 // app.listen(port, () => console.log(`Server is running on port ${port}`));
-
--- frontend/src/components/nav/navbar_container.js
+++ frontend/src/components/nav/navbar_container.js
@@ -1,26 +1,32 @@
-import { connect } from 'react-redux';
-import { logout } from '../../actions/session_actions';
-import NavBar from './navbar.jsx';
-import { openModal } from '../../actions/modal_actions';
+import React from "react";
+import { connect } from "react-redux";
+import { logout } from "../../actions/session_actions";
+import NavBar from "./navbar.jsx";
+import { openModal } from "../../actions/modal_actions";
 
-const mapStateToProps = state => {
-
-    return {
-      user: state.session.user,
-      propic: state.session.user.propic,
-      loggedIn: state.session.isAuthenticated,
-      users: state.entities.users
-    }
+const mapStateToProps = (state) => {
+  return {
+    user: state.session.user,
+    propic: state.session.user.propic,
+    loggedIn: state.session.isAuthenticated,
+    users: state.entities.users,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     logout: () => dispatch(logout()),
-    createPost: (<div><div className="create-post-button" onClick={() => dispatch(openModal('createPost'))}>New Post</div> </div>)
-  }
-}
+    createPost: (
+      <div>
+        <div
+          className="create-post-button"
+          onClick={() => dispatch(openModal("createPost"))}
+        >
+          New Post
+        </div>{" "}
+      </div>
+    ),
+  };
+};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(NavBar);
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
